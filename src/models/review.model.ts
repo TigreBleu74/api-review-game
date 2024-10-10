fe0c33300e849064b5a94b7992f5a50691b477cb
TigreBleu74 <tigrebleu74460@gmail.com>
2024-10-10 17:31:53 +0200

    Exercice 4i

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,19 +1,47 @@
 import { DataTypes, Model } from "sequelize";
-import sequelize from "../config/database"; // Connexion à la base de données
+import sequelize from "../config/database";
+import { Game } from './game.model'; // Connexion à la base de données
 
 export interface ReviewAttributes {
+  id?: number;
+  review_text: string;
+  rating: number;
+  game_id: number;
+  game?: Game;
 }
 
-export class Review
-  extends Model<ReviewAttributes>
-  implements ReviewAttributes
-{
+export class Review extends Model<ReviewAttributes> implements ReviewAttributes {
+  public id!: number;
+  public review_text!: string;
+  public rating!: number;
+  public game_id!: number;
+  public game!: Game;
 }
 
 Review.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    review_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
   {
     sequelize,
     tableName: "reviews",
   }
 );
+
+Review.belongsTo(Game, { foreignKey: "game_id", as: "game" });
